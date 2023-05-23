bb0352d5ba856649d6a5a01106b2705d25c57cf8
Tom Fyfe <tomfyfe85@gmail.com>
2023-05-24 00:00:39 +0100

    installed typescript

---
+++ oddInt.ts
@@ -0,0 +1,10 @@
+export const findOdd = (xs: number[]): number => {
+  for (let nums = 0; 0 < num; nums++) {
+    if (xs.count(nums) > 0 && xs.count(nums) !== % 0) {
+      return xs.count(nums)
+    } else {
+  
+      return 0;
+    };
+  }
+}
