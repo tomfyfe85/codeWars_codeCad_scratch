17bd345d424e9aadf6e64d58e13d15ad55d008b0
Tom Fyfe <tomfyfe85@gmail.com>
2023-05-24 02:36:35 +0100

    added scratch.ts

---
+++ scratch.js
@@ -0,0 +1,7 @@
+var operations = {
+    add: function (a, b) { return a + b; },
+    subtract: function (a, b) { return a - b; },
+    multiply: function (a, b) { return a * b; },
+    divide: function (a, b) { return a / b; },
+};
+console.log(operations["add"](1, 2));
