70ec7f270e7a96c3f15de8487fd63981b0b5bfcd
Tom Fyfe <tomfyfe85@gmail.com>
2023-05-24 16:50:07 +0100

    solution to user input in practice test

--- scratch.js
+++ scratch.js
@@ -1,5 +1,36 @@
-function joinCharacters(N, characterStream) {
-    var joinedString = characterStream.replace(/\s/g, '');
-    console.log(joinedString);
-}
-joinCharacters(4, "o m a r");
+// function joinCharacters(N, characterStream) {
+//     var joinedString = characterStream.replace(/\s/g, '');
+//     console.log(joinedString);
+// }
+// joinCharacters(4, "o m a r");
+
+// const newInput = process.stdin.on("data", (data) => {
+//   const newNum = parseInt(data);
+//   return newNum;
+//   //   process.stdout.write(data + "\n");
+// });
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let number;
+let string;
+
+rl.question("", (numInput) => {
+  number = parseInt(numInput);
+
+  rl.question("", (strInput) => {
+    string = strInput;
+
+    function joinCharacters(num, str) {
+      var joinedString = str.replace(/\s/g, "");
+      return joinedString;
+    }
+    console.log(joinCharacters(number, string));
+    rl.close();
+  });
+});
